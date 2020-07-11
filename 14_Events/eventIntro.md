e19a64deab41117a35652922e3699c21e884058c
changhyun <jchangh2@gmail.com>
2020-07-12 01:48:23 +0900

    event listener add/remove

--- 14_Events/eventIntro.md
+++ 14_Events/eventIntro.md
@@ -14,3 +14,17 @@ addEventListener('..', event=>...);
 addEventListener('..', event=>...);
 
 data를 전달.
+
+## Browser event Objects
+
+event
+  - MouseEvent
+    - coordinates
+    - event target
+  - DragEvent
+    - extra data
+    - event target
+  - KeyEvent
+  - ...
+    - event target (common)
+
---
+++ 14_Events/events-01-starting-setup/events-01-starting-setup/assets/scripts/events.js
@@ -0,0 +1,43 @@
+const button = document.querySelector("button");
+
+button.onclick = function () {
+  console.log("hi");
+};
+
+const btnClickHandler = () => {
+  alert("button was clicked!");
+};
+const anotherButtonClickHandler = () => {
+  console.log("this was clicked!");
+};
+
+// onClick : 중첩이 안 됨.
+button.onclick = btnClickHandler;
+button.onclick = anotherButtonClickHandler;
+
+// addEventListener 중첩 가능, 삭제 가능
+button.addEventListener("click", btnClickHandler);
+button.addEventListener("click", anotherButtonClickHandler);
+button.removeEventListener("click", anotherButtonClickHandler);
+
+setTimeout(() => {
+  button.removeEventListener("click", btnClickHandler);
+}, 5000);
+
+// 지워지지 않아 why? >> 서로 다른 함수라서.
+button.addEventListener("click", () => {
+  console.log("hi");
+});
+button.removeEventListener("click", () => {
+  console.log("hi");
+});
+
+// 마찬가지
+button.addEventListener("click", btnClickHandler.bind(this));
+button.removeEventListener("click", btnClickHandler.bind(this));
+
+// 이건 돼.
+const boundFn = btnClickHandler.bind(this);
+
+button.addEventListener("click", boundFn);
+button.removeEventListener("click", boundFn);
